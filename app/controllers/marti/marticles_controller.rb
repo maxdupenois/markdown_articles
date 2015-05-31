17801dbbe4afda60ddbc844faa5af3464e66447b
Max <max.dupenois@forward.co.uk>
2015-05-31 12:26:11 +0100

    Better article not found response

--- app/controllers/marti/marticles_controller.rb
+++ app/controllers/marti/marticles_controller.rb
@@ -6,13 +6,27 @@ module Marti
     end
 
     def show
-      path = params[:path]
-      render :show, locals: { marticle: Marticle.build(path), marticles: marticles }
-    rescue ArticleNotFoundException => _
-      redirect_to "/404", status: 404
+      render :show, locals: { marticle: marticle, marticles: marticles }
+    rescue Errors::ArticleNotFoundError => _
+      not_found!
     end
 
     private
+
+    def not_found!
+      redirect_path = Marti.article_not_found_path
+      redirect_path << (redirect_path =~ /\?/ ? '&' : '?')
+      redirect_path << "article=#{params[:path]}"
+      redirect_to redirect_path, status: 404
+    end
+
+    def marticle
+      @martcile ||= builder.build
+    end
+
+    def builder
+      @builder ||= Marti::MarticleBuilder.new(params[:path])
+    end
 
     def marticles
       @marticles ||= Marticle.articles
